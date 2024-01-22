72fbe478ec6b50dbfd64bf793c94a2aafece8b8d
Serge Malo <sergemalo2000@outlook.com>
2024-01-22 15:43:14 -0500

    Added Functions and Scope

--- section_7/src/main.rs
+++ section_7/src/main.rs
@@ -1,6 +1,32 @@
+
+// Global Variables are unsafe
+static mut  MY_GLOBAL:i32 = 666;
+
 fn main() {
     
     // FUNCTIONS
+    say_hello("Alice");
+    say_hello("Bob");
+    
+    let mut name = "Alice";
+    change_name(&mut name);
+    say_hello(&name);
 
+    // Scoping	
+    {
+        let x = 5;
+        println!("x = {}", x);
+    }
+    
+    //println!("my_global = {}", MY_GLOBAL);
+    // Use unsafe keyword - Not really recommanded...
+    unsafe { println!("my_global = {}", MY_GLOBAL); }
+}
 
+fn say_hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+fn change_name(name: &mut &str) {
+    *name = "John";
 }
