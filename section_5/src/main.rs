644a69c172fb9ad82048b28fd0029cd4cc7b8f78
Serge Malo <sergemalo2000@outlook.com>
2024-01-22 11:29:25 -0500

    Completed Slices

--- section_5/src/main.rs
+++ section_5/src/main.rs
@@ -1,3 +1,5 @@
+//use core::slice::SlicePattern;
+
 
 
 const MY_CONST: i32 = 5;
@@ -39,4 +41,43 @@ fn main() {
 
     // Slices
 
+    // A reference to a contiguous sequence of elements in a vector
+    let v = vec![1, 2, 3, 4, 5];
+    let slice = &v[1..3];
+    println!("slice: {:?}", slice);
+    // Size of slice is know at runtime
+    println!("size of slice: {}", slice.len());
+    // Can be used on arrays, vectors and Strings
+    // Are indexed
+
+    // Slices TO MUTABLE ARRAYS
+    // Slices to mutable arrays allow us to change elements - not the size
+    // First define a mutable array
+    let mut colors = ["red", "green", "blue"];
+    // As a mutable array, we can change the elements.
+    colors[1] = "yellow";
+    println!("colors: {:?}", colors);
+    // 
+    // Define a slice to that mutable array
+    // Note that the slice itself is not mutable - it is a reference to a mutable array
+    let slice_to_arr = &mut colors[1..3];
+    slice_to_arr[1] = "orange";
+    println!("colors: {:?}", colors); // AFTER THIS CALL, We can't use the slice slice_to_arr anylonger
+    // Do the same, with a function
+    // 3 different ways to do that:
+    update_colors(&mut colors[1..3]); // cast to a slice to mutable array
+    println!("colors: {:?}", colors);
+    colors[2] = "orange";
+    update_colors(colors[1..3].as_mut()); // use as_mut()
+    println!("colors: {:?}", colors);
+    colors[2] = "orange";
+    let slice_to_arr2 = &mut colors[1..3];
+    update_colors(slice_to_arr2); // use the defined slice
+    println!("colors: {:?}", colors);
+    
+
+}
+
+fn update_colors(colors_slice: &mut [&str]) {
+    colors_slice[1] = "brown";
 }
