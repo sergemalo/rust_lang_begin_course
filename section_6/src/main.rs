cd1a46ce6b122e2a197d39538bb47301848ba95a
Serge Malo <sergemalo2000@outlook.com>
2024-01-22 13:57:51 -0500

    Added if statement

--- section_6/src/main.rs
+++ section_6/src/main.rs
@@ -1,5 +1,20 @@
+use rand::Rng;
+
 fn main() {
     
     // IF
+    let mut rng = rand::thread_rng();
+    let x = rng.gen_range(0, 10);
+    println!("X: {}", x);
+    if x < 3 {
+        println!("< 3");
+    } else if x < 6{
+        println!("< 6");
+    } else {
+        println!(">= 6");
+    }
+    // One-Liner definition
+    let y = if x < 5 { true } else { false };
+    println!("Y: {}", y);
 
 }
