fff321169915db49d74d6027c32bb34c84622144
Serge Malo <sergemalo2000@outlook.com>
2024-01-22 14:11:30 -0500

    Added Match

--- section_6/src/main.rs
+++ section_6/src/main.rs
@@ -24,12 +24,24 @@ fn main() {
         2 => println!("Two"),
         _ => println!("Other")
     }       // ===> NO SEMICOLON!
-    // Match can return a value
+    // Match can return a value (and show how we can use "pattern matching" in that case)
     let y = match x {
         0 => "Zero",
         1 => "One",
-        2 => "Two",
+        2 | 3 => "Two or Three",
+        4..=7 => "Four to Seven",
+        _ if (x % 2) == 0 => "Even",
         _ => "Other"
     };        // ===> SEMICOLON!
     println!("Y: {}", y);
+
+    // "Pattern matching"
+    let point = (rng.gen_range(0, 2), rng.gen_range(0, 2));
+    println!("Point: {:?}", point);
+    match point {
+        (0, 0) => println!("Origin"),
+        (x, 0) => println!("On the X axis at X: {}", x),
+        (0, y) => println!("On the Y axis at Y: {}", y),
+        (x, y) => println!("Point at ({}, {}) ", x, y)
+    }
 }
