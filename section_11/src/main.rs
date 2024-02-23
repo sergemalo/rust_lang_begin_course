0a17f2faa0deec2a4c2dd4d6f6eb8729b1c68121
Serge Malo <sergemalo2000@outlook.com>
2024-02-23 11:33:39 -0500

    Added threads

--- section_11/src/main.rs
+++ section_11/src/main.rs
@@ -1,3 +1,25 @@
+use std::thread;
+use std::time::Duration;
+use std::thread::sleep;
+// Rust ownership/borrowing:
+// * Memory safety
+// * No Data races
+
 fn main() {
+    let mut threads = vec![];
+
+    for i in 0..10 {
+        let th = thread::spawn(move || {
+            println!("New thread: {}", i);
+            sleep(Duration::from_secs(1));
+            println!("New thread: {} DONE!", i);
+        });
+        threads.push(th);
+    }
+
+    // Waiting for all threads to finsih: calling join
+    for t in threads {
+        t.join().unwrap();
+    }
     println!("Hello, world!");
 }
