276914d6a18a35cbaf689c9daa5ebfcb73ea667c
Serge Malo <sergemalo2000@outlook.com>
2024-02-23 15:26:46 -0500

    Added Mutex

--- section_11/src/main.rs
+++ section_11/src/main.rs
@@ -2,6 +2,8 @@ use std::thread;
 use std::time::Duration;
 use std::thread::sleep;
 use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 // Rust ownership/borrowing:
 // * Memory safety
 // * No Data races
@@ -9,6 +11,12 @@ use std::sync::mpsc;
 // Channels:
 // a Way to send data between threads
 // MPSC:  Multiple producer, single consumer
+
+// Mutex:
+// ARC:   Atomic Referenced counted type
+// Convert data to primitive types - access is thread safe
+// lock is blockig, try_lock is not
+// Poised lock: when a thread that holds the lock panics - lock_is_poisoned will return true
 
 const NB_THREADS: usize = 5; // usize is used for calling thread methods (local system dependent)
 
@@ -27,29 +35,9 @@ fn main() {
 
     basic_channel_example();
 
-    let (tx, rx) = mpsc::channel();
-    for i in 0..NB_THREADS {
-        let tx = tx.clone();
-        start_thread(i, tx);
-    }
-    // IMPORTANT: When you use rx.iter() without take(NB_THREADS), 
-    // the iter() method creates an iterator over the elements of the channel, 
-    // and since the channel is asynchronous, it will keep waiting for new elements to arrive indefinitely. 
-    // This can lead to an infinite loop if the channel remains open and continues to receive new elements without a limit on the iteration.    
-    for i in rx.iter().take(NB_THREADS) {       // Without "take" it would be infinite
-        println!("Received: {}", i);
-    }
-}
+    basic_channel_example2();
 
-fn basic_channel_example() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let msg = String::from("Hello from thread");
-        tx.send(msg).unwrap();
-    });
-    let msg = rx.recv().unwrap(); // recv is a blocking call
-    println!("Received: {}", msg);
-    
+    mutex_example();
 }
 
 fn main_for_threads() {
@@ -69,4 +57,53 @@ fn main_for_threads() {
         t.join().unwrap();
     }
     println!("Main thread completes!");
+}
+
+
+fn basic_channel_example() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let msg = String::from("Hello from thread");
+        tx.send(msg).unwrap();
+    });
+    let msg = rx.recv().unwrap(); // recv is a blocking call
+    println!("Received: {}", msg);
+    
+}
+
+fn basic_channel_example2() {
+    let (tx, rx) = mpsc::channel();
+    for i in 0..NB_THREADS {
+        let tx = tx.clone();
+        start_thread(i, tx);
+    }
+    // IMPORTANT: When you use rx.iter() without take(NB_THREADS), 
+    // the iter() method creates an iterator over the elements of the channel, 
+    // and since the channel is asynchronous, it will keep waiting for new elements to arrive indefinitely. 
+    // This can lead to an infinite loop if the channel remains open and continues to receive new elements without a limit on the iteration.    
+    for i in rx.iter().take(NB_THREADS) {       // Without "take" it would be infinite
+        println!("Received: {}", i);
+    }  
+}
+
+fn mutex_example() {
+
+    let c = Arc::new(Mutex::new(0));
+
+    let mut threads = vec![];
+
+    for _i in 0..10 {
+        let c = Arc::clone(&c);
+        let t = thread::spawn(move || {
+            let mut num = c.lock().unwrap();
+            *num += 1;
+            drop(num); // Release the lock manually, not required because of Arc RAII
+        });
+        threads.push(t);
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
+    println!("Counter: {}", *c.lock().unwrap());
 }
